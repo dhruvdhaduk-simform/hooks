dd2fc127d8261e813e352a899f2bec3acdfa3705
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-19 16:04:34 +0530

    Create the demo for useToggle hook

--- src/demos/UseToggleDemo.tsx
+++ src/demos/UseToggleDemo.tsx
@@ -1,9 +1,26 @@
 import { SectionHeading } from '@/components/SectionHeading';
+import { Button } from '@/components/ui/Button';
+import { useToggle } from '@/hooks/useToggle';
 
 export function UseToggleDemo() {
+    const [value, toggle, setValue] = useToggle();
+
     return (
-        <div>
+        <div className="p-4">
             <SectionHeading label="useToggle demo" />
+            <div className="p-4">
+                <div className="p-1 flex justify-center">
+                    <Button onClick={toggle}>
+                        Value is {JSON.stringify(value).toUpperCase()}
+                    </Button>
+                </div>
+                <div className="p-1 flex justify-center gap-2">
+                    <Button onClick={() => setValue(true)}>Set to true</Button>
+                    <Button onClick={() => setValue(false)}>
+                        Set to false
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 }

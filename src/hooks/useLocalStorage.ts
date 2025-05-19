9872d6920886bc1f0a3162392859689624d36606
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-05-19 16:38:17 +0530

    Create the 'useLocalStorage' hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export function useLocalStorage<T>(
+    localStorageKey: string,
+    initialValue: T,
+    parser: (value: string) => T
+) {
+    let storedValue: T = initialValue;
+    try {
+        const storedValueRaw = localStorage.getItem(localStorageKey);
+        if (storedValueRaw) {
+            storedValue = parser(storedValueRaw);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+
+    const [value, setValue] = useState<T>(storedValue);
+
+    useEffect(() => {
+        localStorage.setItem(localStorageKey, JSON.stringify(value));
+    }, [value, localStorageKey]);
+
+    const removeValue = useCallback(
+        (syncState: boolean = true) => {
+            localStorage.removeItem(localStorageKey);
+            if (syncState) setValue(initialValue);
+        },
+        [localStorageKey, initialValue]
+    );
+
+    return [value, setValue, removeValue] as const;
+}
